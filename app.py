cca454c1a6b3749770b3278d192f25199317704f
AKindWorld <senarkopal@gmail.com>
2020-12-20 23:51:56 +0530

    added a brand new and not-so-shiny 404 page for all the lost travellers

--- app.py
+++ app.py
@@ -11,7 +11,11 @@ app = Flask(__name__)
 @app.route('/landingpage')
 def landingpage():
     return render_template("index.html")
-
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':

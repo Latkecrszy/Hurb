1b5df9e86b4818a1f4a63bb80ac2470718acc779
AKindWorld <senarkopal@gmail.com>
2020-12-21 14:53:27 +0530

    footer links can now move & groove too!

--- app.py
+++ app.py
@@ -11,10 +11,9 @@ app = Flask(__name__)
 @app.route('/landingpage')
 def landingpage():
     return render_template("index.html")
-    
+
 @app.errorhandler(404)
 def page_not_found(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
 

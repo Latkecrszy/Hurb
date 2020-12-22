08ba8b4ac37ad4dfa19d88e6908c6345f52bec97
Seth Raphael <setharaphael7@gmail.com>
2020-12-22 13:35:04 -0800

    added commands path

--- app.py
+++ app.py
@@ -10,11 +10,17 @@ app = Flask(__name__)
 
 @app.route('/landingpage')
 def landingpage():
-    return render_template("index.html")
+    return render_template("index.html"), 200
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/commands')
+def commands():
+    return render_template("commands.html", commands=json.load(open("commands.json"))), 200
 
 
 if __name__ == '__main__':
